2e51a0ebb620a8aeb64e20b4e733ee6f1f931b09
the1gamerdude <cole.kuehmann@me.com>
2013-04-28 21:39:02 -0700

    mcmoc and other .java files

---
+++ common/nam/the1gamerdude/Nature_and_materials.java
@@ -0,0 +1,29 @@
+package nam.the1gamerdude;
+
+import nam.the1gamerdude.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class Nature_and_materials {
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ common/nam/the1gamerdude/lib/Reference.java
@@ -0,0 +1,8 @@
+package nam.the1gamerdude.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "NAM";
+    public static final String MOD_NAME = "Nature and Materials";
+    public static final String VERSION = "0.0.1";
+}
